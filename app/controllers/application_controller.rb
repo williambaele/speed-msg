5d77594c07bc4c6031006cd29579d52a2bc7f0b5
William Baele <baelewilliam1999@gmail.com>
2023-02-21 14:35:06 +0100

    login and sign up pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[pseudo])
 
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[pseudo])
+  end
+
+  def after_sign_in_path_for(_resource)
+    root_path
+  end
 end
