ceb7012ae77c64f1c283ee3c217560249b4ac6e6
William Baele <baelewilliam1999@gmail.com>
2023-02-21 14:44:10 +0100

    login and sign up pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  add_flash_types :info, :error, :success
+  before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[pseudo])
@@ -10,4 +13,16 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(_resource)
     root_path
   end
+
+  def after_sign_up_path_for(resource)
+    flash[:success] = "Account created and signed in"
+    sign_in(resource)
+    root_path
+  end
+
+  def after_sign_in_path_for(resource)
+    flash[:success] = "Signed in"
+    root_path
+  end
+
 end
